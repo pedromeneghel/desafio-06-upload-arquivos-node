66a8608ab601b68928940635f8d138ea40256130
Pedro Meneghel <pedromeneghel@gmail.com>
2020-05-17 23:18:47 -0300

    Models Finished

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,16 @@
+import { Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @CreateDateColumn()
   updated_at: Date;
 }
 
